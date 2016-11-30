05e395ff44512383f4e1d207cf3d383ace03036b
md10 <md10@SJ211-05.lab.infnet.com.br>
2016-11-30 19:01:08 -0200

    Feito Erro, e algoritmo Runge Kutta de Quarta Ordem

---
+++ src/main/rungekutta/RKpontoMedioQuartaOrdem.java
@@ -0,0 +1,55 @@
+package main.rungekutta;
+
+import java.text.DecimalFormat;
+
+import main.funcoes.Funcoes;
+
+/**
+ * Página 626 do PDF -- Algoritmo de RK de segunda ordem
+ * @author md10
+ *
+ */
+public class RKpontoMedioQuartaOrdem {
+	public static void main(String[] args) {
+		double y0 = 1, yv;
+		double h = 0.5;		//(b - a) / n;
+		double yi = 1;
+		
+		System.out.println("x\t yVerd\t\ty\terro");
+		for (double i = 0; i <= 4.0; i += h) {
+			yv = Funcoes.f4(i);
+			
+			double k1 = Funcoes.f3(i, y0);
+			double k2 = Funcoes.f3(i + h/2, y0 + (k1*h)/2);
+			double k3 = Funcoes.f3(i + h/2, y0 + (k2*h)/2);
+			double k4 = Funcoes.f3(i + h, y0 + k3*h);
+			
+			double yant = yi; // y Antes de calcular yi
+			yi = yi + 1.0/6.0 * (k1 + 2*k2 + 2*k3 + k4) * h;
+			
+			print(i, yv, yant, ((yv - yant) / yv) * 100);
+		}
+	}
+	
+	private static void print(double i, double yv, double yant,
+			double error) {
+		
+		DecimalFormat df = new DecimalFormat("0.00");
+		String errorString = df.format(error);
+		
+		String output;
+		if (String.valueOf(yv).length() > 6) {
+			output = i + "\t " + yv + "\t";
+		} else {
+			output = i + "\t " + yv + "\t\t";
+		}
+		
+		if (String.valueOf(yant).length() > 6) {
+			output = output + yant + "\t" + errorString + "%";
+		} else {
+			output = output + yant + "\t" + errorString + "%";
+		}
+		
+		System.out.println(output);
+	}
+}
